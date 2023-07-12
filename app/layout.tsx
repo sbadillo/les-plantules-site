0c2730f7d99898dfc47a7fa0bfc872785c055f3d
sbadillo <sbadillo@gmail.com>
2023-07-13 00:31:55 +0200

    remove script from root layout body, remove load strategy

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,11 @@ export default function RootLayout({
     <html lang="en">
         <Script 
         src="https://identity.netlify.com/v1/netlify-identity-widget.js" 
-        strategy="beforeInteractive"
+        // strategy="beforeInteractive"
         />
       <body className={inter.className}>
         {children}
-        <Script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js" />
+        {/* <Script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js" /> */}
       </body>
     </html>
   );
