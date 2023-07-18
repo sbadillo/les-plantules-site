b4ad20c60dada9a5e12ddabd4821381b4b182072
sbadillo <sbadillo@gmail.com>
2023-07-19 00:04:49 +0200

    created mdx setup for custom mdx components

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,8 +1,8 @@
-// app/posts/[slug]/page.tsx
 import { format, parseISO } from 'date-fns'
 import { allPosts } from 'contentlayer/generated'
-import { useMDXComponent } from 'next-contentlayer/hooks'
 import { notFound } from 'next/navigation'
+import { Mdx } from "components/mdx";
+import Balancer from 'react-wrap-balancer';
 
 export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
 
@@ -19,19 +19,18 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   // 404 if the post does not exist.
   if (!post) notFound()
 
-  // Parse the MDX file via the useMDXComponent hook.
-  const MDXContent = useMDXComponent(post.body.code)
-
   return (
     <article className="mx-auto max-w-xl py-8">
       <div className="mb-8 text-center">
         <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
           {format(parseISO(post.date), 'LLLL d, yyyy')}
         </time>
-        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <h1 className="text-3xl font-bold">
+        <Balancer>{post.title}</Balancer>
+        </h1>
       </div>
-      {/* <div className="[&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} /> */}
-      <MDXContent />
+      {/* <MDXContent /> */}
+      <Mdx code={post.body.code} />
     </article>
   )
 }
